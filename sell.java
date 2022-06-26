2ec3a4952ad827532ac976691eaa486e1f3f6646
Deepak kumar <61027679+Deepm9tkjl@users.noreply.github.com>
2022-06-26 23:10:50 +0530

    Update sell.java

--- sell.java
+++ sell.java
@@ -1,70 +1,31 @@
+package softnerv;
 
-import java.util.*;
-
- public class sell {
-	static int fmp(int[] prices, int i, int k,
-								int buy, int[][] v)
-	{
-
-		if (i >= prices.length || k <= 0)
-			return 0;
-
-		if (v[i][buy] != -1)
-			return v[i][buy];
-
-		int nbuy;
-		if (buy == 1)
-			nbuy = 0;
-		else
-			nbuy = 1;
-		if (buy == 1) {
-			return v[i][buy] = Math.max(
-					-prices[i]
-						+fmp(
-							prices, i + 1, k, nbuy, v),
-					fmp(prices, i + 1, k,
-										(int)(buy), v));
-		}
+public class buy {
 
 
-		else {
+static int maxProfit(int prices[], int n)
+{
+	int buy = prices[0], max_profit = 0;
+	for (int i = 1; i < n; i++) {
 
-		
-			if (buy == 1)
-				nbuy = 0;
-			else
-				nbuy = 1;
-			return v[i][buy]
-				= Math.max(
-					prices[i]
-						+ fmp(prices, i + 1,
-											k - 1, nbuy, v),
-					fmp(prices, i + 1, k, buy,
-									v));
-		}
+	
+	if (buy > prices[i])
+		buy = prices[i];
+
+	
+	else if (prices[i] - buy > max_profit)
+		max_profit = prices[i] - buy;
 	}
-	static int maxProfit(int[] prices)
-	{
-		int n = prices.length;
-		int[][] v = new int[n][2];
+	return max_profit;
+}
 
-		for (int i = 0; i < v.length; i++) {
-			v[i][0] = -1;
-			v[i][1] = -1;
-		}
-
-		return fmp(prices, 0, 1, 1, v);
-	}
-
-	public static void main(String[] args)
-	{
-
-		int[] prices = { 7,1,5,3,6,4 };
-
-		
-		int ans = maxProfit(prices);
-
-		System.out.println(ans);
-	}}
+public static void main(String args[])
+{
+	int prices[] = { 7, 1, 5, 6, 4 };
+	int n = prices.length;
+	int max_profit = maxProfit(prices, n);
+	System.out.println(max_profit);
+}
+}
 
 
