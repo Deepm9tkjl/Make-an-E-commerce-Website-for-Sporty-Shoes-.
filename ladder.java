405b4794d0ddbf416266391de8c3c5bd18b63ddf
Deepak kumar <61027679+Deepm9tkjl@users.noreply.github.com>
2022-06-26 23:13:34 +0530

    Update and rename Leader.java to ladder.java

---
+++ ladder.java
@@ -0,0 +1,29 @@
+package softnerv;
+
+public class ladder {
+
+	
+	void printLeaders(int arr[], int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int j;
+			for (j = i + 1; j < size; j++)
+			{
+				if (arr[i] <=arr[j])
+					break;
+			}
+			if (j == size) 
+				System.out.print(arr[i] + " ");
+		}
+	}
+
+	
+	public static void main(String[] args)
+	{
+		ladder lead = new ladder();
+		int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+		int n = arr.length;
+		lead.printLeaders(arr, n);
+	}
+}
