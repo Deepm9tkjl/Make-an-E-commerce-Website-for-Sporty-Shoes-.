07f9b8687758e5e8f6ce1ad450f987446af85600
Deepak kumar <61027679+Deepm9tkjl@users.noreply.github.com>
2022-06-26 23:12:35 +0530

    Update xor.java

--- xor.java
+++ xor.java
@@ -1,26 +1,38 @@
-class xor {
-     
-    
-    static int xorSum(int arr[], int n)
-    {
-         
-        int bits = 0;
+package softnerv;
 
-        for (int i = 0; i < n; ++i)
-            bits |= arr[i];
-     
-        int ans = bits * (int)Math.pow(2, n-1);
-     
-        return ans;
-    }
-     
- //xor
-    public static void main(String[] args)
-    {
-         
-        int arr[] = {1,3};
-        int size = arr.length;
-         
-        System.out.print(xorSum(arr, size));
-    }
+public class xor {
+
+	static int rec(int i, int x, int arr[], int size)
+	{
+	
+	
+		if (i == size)
+			return x;
+	
+		int choice1 = rec(i + 1, x ^ arr[i], arr, size);
+
+		
+		int choice2 = rec(i + 1, x, arr, size);
+
+		
+		return choice1 + choice2;
+	}
+
+
+	static int xorSum(int arr[], int size)
+	{
+		return rec(0, 0, arr, size);
+	}
+
+
+	public static void main(String[] args)
+	{
+		int arr[] = { 1, 5, 6 };
+		int size = arr.length;
+	
+	
+		System.out.println(xorSum(arr, size));
+	}
 }
+
+
